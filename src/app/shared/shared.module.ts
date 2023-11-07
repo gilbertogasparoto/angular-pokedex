2be13754b51ff25b4373a3cc9e39f46cca8ed44e
unknown <gilbertohfgasparoto@gmail.com>
2023-11-07 08:18:16 -0300

    Start do projeto, configurando rotas e os primeiros componentes

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+
+@NgModule({
+  declarations: [HeaderComponent, SearchComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
