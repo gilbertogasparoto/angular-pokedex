cf091a593db4ea8b5373c554e06b13ac8e197bb0
unknown <gilbertohfgasparoto@gmail.com>
2023-11-07 08:46:06 -0300

    Estilizando componente de pesquisa

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SearchComponent],
-  exports: [HeaderComponent],
+  declarations: [HeaderComponent, SearchComponent, ListComponent],
+  exports: [HeaderComponent, SearchComponent, ListComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
